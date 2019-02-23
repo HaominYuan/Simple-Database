64a02f829ec3f5e77c2300d24c2e34a1cad45d35
haominyuan <tstxxy@gmail.com>
2019-02-23 14:22:48 +0800

    简单的SQL解释器和虚拟机

--- main.c
+++ main.c
@@ -8,12 +8,41 @@ struct InputBuffer_t {
     size_t buffer_length;
     ssize_t input_length;
 };
-
 typedef struct InputBuffer_t InputBuffer;
+
+enum MetaCommandResult_t {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetaCommandResult;
+
+enum PrepareResult_t {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+};
+typedef enum PrepareResult_t PrepareResult;
+
+enum StatementType_t {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+};
+typedef enum StatementType_t StatementType;
+
+struct Statement_t {
+    StatementType type;
+};
+typedef struct Statement_t Statement;
+
 
 InputBuffer *new_input_buffer(void);
 void print_prompt(void);
 void read_input(InputBuffer *input_buffer);
+MetaCommandResult do_meta_command(InputBuffer *input_buffer);
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+void execute_statement(Statement *statement);
+
+
+
 
 int main(int argc, char *argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
@@ -21,13 +50,28 @@ int main(int argc, char *argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            /* 定义在stdlib.h 头文件中 #define EXIT_SUCCESS 0 */
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'. \n", input_buffer->buffer);        }
-    }
+	if (input_buffer->buffer[0] == '.') {
+	    switch (do_meta_command(input_buffer)) {
+	        case (META_COMMAND_SUCCESS):
+		    continue;
+		case (META_COMMAND_UNRECOGNIZED_COMMAND):
+		    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+		    continue;
+	    }    
+	}
 
+	Statement statement;
+	switch (prepare_statement(input_buffer, &statement)) {
+	    case (PREPARE_SUCCESS):
+	        break;
+	    case (PREPARE_UNRECOGNIZED_STATEMENT):
+	        printf("Unrecognized keyword at start of '%s'\n", input_buffer->buffer);
+		continue;
+	}
+
+	execute_statement(&statement);
+	printf("Executed.\n");
+    }
     return 0;
 }
 
@@ -64,3 +108,36 @@ void read_input(InputBuffer* input_buffer) {
 }
 
 
+MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+	return PREPARE_SUCCESS;
+    }
+
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+	return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+	    printf("This is where we would do an insert.\n");
+	    break;
+	case (STATEMENT_SELECT):
+	    printf("This is where we would do a select.\n");
+	    break;
+    }
+}
